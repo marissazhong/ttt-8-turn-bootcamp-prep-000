71574b9579404374c139e5c05c30af4244095d09
Marissa Zhong <mwz2101@columbia.edu>
2018-06-23 22:44:12 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,7 +32,7 @@ def move(board,index,player = "X")
   board[index] = player
 end
 
-def turn(board,index)
+def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
