d4beda82a9b340c7e7021c4fb92e75c19355084f
Marissa Zhong <mwz2101@columbia.edu>
2018-06-23 22:35:39 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,12 @@
 
 
-
+def valid_move?(board, index)
+  if position_taken?(board,index) == FALSE && index < 9 && index >= 0
+    TRUE
+  else
+    FALSE
+  end
+end
 
 def position_taken?(board,index)
   if board[index] == " " || board[index] == "" || board[index] == nil
