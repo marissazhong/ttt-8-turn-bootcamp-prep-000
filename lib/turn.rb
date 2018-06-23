54f54fdd2ceb305fa3512a27e95547b1e7676b36
Marissa Zhong <mwz2101@columbia.edu>
2018-06-23 22:36:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,3 +15,19 @@ def position_taken?(board,index)
     TRUE
   end
 end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i-1
+end
+
+def move(board,index,player = "X")
+  board[index] = player
+end
